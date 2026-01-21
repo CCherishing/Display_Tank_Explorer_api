19efe91c16bd14da1865dfb4c0607fc09c174fce
bilbo <bill.bao3052@gmail.com>
2026-01-21 02:00:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from creatures import load_species_from_csv
+from species import load_species_from_csv
 import os
 
 Base_Dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,16 +13,23 @@ species = load_species_from_csv(species_csv)
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/creatures")
-def get_creatures():
+@app.route("/species")
+def get_species():
     return jsonify(species)
 
-@app.route("/creatures/<creature_id>")
-def get_creature(creature_id):
+@app.route("/species/<species_id>")
+def get_specie(species_id):
     for c in species:
-        if c["id"] == creature_id:
+        if c["id"] == species_id:
             return jsonify(c)
     return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "ok",
+        "api": "Display Tank Explorer"
+    })
